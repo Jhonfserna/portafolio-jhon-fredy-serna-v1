eea8ba9193049047e7fd8e7ea49dd68ba715d3da
Jhon Serna <jhonfserna@yahoo.com>
2024-08-16 21:13:34 -0500

    Actualización de datos al 16 de agosto del 2024

--- validacion.js
+++ validacion.js
@@ -1,27 +1,26 @@
-function validarDatos(){
+function validarDatos() {
     window.event.preventDefault()
-     
-    if (document.form.nombre.value=="" ) {
-        alert("Campo nombre es obligatorio")  
+
+    if (document.form.nombre.value == "") {
+        alert("Campo nombre es obligatorio")
         document.form.nombre.focus()
-    
-    }else if (document.form.email.value=="") {
+
+    } else if (document.form.email.value == "") {
         alert("Campo e-mail es obligatorio")
-        document.form.email.focus() 
+        document.form.email.focus()
 
-    }else if (document.form.asunto.value=="" ) {
+    } else if (document.form.asunto.value == "") {
         alert("Campo Asunto es obligatorio")
-        document.form.asunto.focus() 
+        document.form.asunto.focus()
 
-    }else if (document.form.mensaje.value=="" || document.form.mensaje.value.length <= 50 ){
-        alert("Campo Mensaje es obligatorio y debe contener máximo 50 carateres") 
+    } else if (document.form.mensaje.value == "" || document.form.mensaje.value.length <= 1000) {
+        alert("Campo Mensaje es obligatorio y debe contener máximo 1.000 carateres")
         document.form.mensaje.focus()
-      
-    } else if (document.form.email.value.indexOf('@')==-1 ||
-    document.form.email.value.indexOf('.')==-1 ) {
+    } else if (document.form.email.value.indexOf('@') == -1 ||
+        document.form.email.value.indexOf('.') == -1) {
         alert("e-mail inválido")
     }
-   
+
 }
-    document.querySelector('form').addEventListener('submit',validarDatos)
-  
+document.querySelector('form').addEventListener('submit', validarDatos)
+
